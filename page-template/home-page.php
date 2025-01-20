7128199196cae2caaa929b0e706fd7088968a506
Shuvo Islam <shuvoislamcmt3@gmail.com>
2025-01-20 14:23:41 +0600

    create homepage page template start markup

---
+++ page-template/home-page.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Template Name: Home Page
+ */
+?>
+<?php
+get_header();
+?>
+
+<section class="hero-area">
+    <div class="container">
+        <div class="row">
+            <h1>Build Your Online Presence</h1>
+            <p>We are a Digital Marketing Agency skilled in
+            Online Marketing Strategies, Web Design and Web Development</p>
+            <a href="" class="hero-btn">Get A Free SEO Audit</a>
+        </div>
+    </div>
+</section>
+
+
+<?php get_footer(); ?>
