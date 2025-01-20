af61138e1c9d53992471b28f0486d534e60f7f84
Shuvo Islam <shuvoislamcmt3@gmail.com>
2025-01-20 20:33:00 +0600

    trusted and experience area markup

--- page-template/home-page.php
+++ page-template/home-page.php
@@ -127,5 +127,79 @@ get_header();
     </div>
 </section>
 
+<!-- experience section start  -->
+
+<section class="experience-area">
+    <div class="container">
+        <div class="row">
+            <h3>Welcome to Webby360</h3>
+            <h2>Years of experience giving Digital Solution to clients</h2>
+        </div>
+        <div class="row">
+            <div class="col">
+                <div class="experience-item">
+                    <h2>200+</h2>
+                    <p>Projects done</p>
+                </div>
+            </div>
+            <div class="col">
+                <div class="experience-item">
+                    <h2>98%</h2>
+                    <p>Customer satisfaction rate.</p>
+                </div>
+            </div>
+            <div class="col">
+                <div class="experience-item">
+                    <h2>4.9-star</h2>
+                    <p>Rated by 100+ Verified Google Reviews</p>
+                </div>
+            </div>
+            <div class="col">
+                <div class="experience-item">
+                    <h2>24/7</h2>
+                    <p>Available</p>
+                </div>
+            </div>
+        </div>
+
+        <img src="<?php echo get_template_directory_uri();?>/images/experience image.jpg" alt="">
+
+    </div>
+</section>
+
+
+<!-- CTA section start  -->
+
+<section class="cta-area">
+    <div class="container text-center">
+        <div class="row align-content-center">
+            <h2>Enhance your Digital Presence
+            with Webby360</h2>
+            <a href="#" class="cta-btn">Get A Free SEO Audit</a>
+        </div>
+    </div>
+</section>
+
+<!-- Testimonial section start  -->
+
+<section class="testimonial">
+
+</section>
+
+<!-- Trusted section start  -->
+
+<section class="trusted-area" style="background-image: url(<?php echo get_template_directory_uri();?>/images/trusted-area-bg.png); background-size:cover;">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h3>Bring in the best</h3>
+                <h2>Most trusted around the globe</h2>
+            </div>
+            <div class="col">
+                <p>Webby360 is a marketing solutions service that will help your business define and execute your marketing goals. We specialize in internet marketing, digital marketing, SEO, social media management, brand strategy and website development. Our team can solve your marketing obstacles and help your business gain targeted leads.</p>
+            </div>
+        </div>
+    </div>
+</section>
 
 <?php get_footer(); ?>
